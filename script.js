6c561b964d4068bd8f42e1decdbcd341fc8d1a29
pergvzt <47556243+pergvzt@users.noreply.github.com>
2024-10-04 06:29:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function normalizeColor(hexCode) {
+function normalizeColor(hexCode) { 
     return [(hexCode >> 16 & 255) / 255, (hexCode >> 8 & 255) / 255, (255 & hexCode) / 255]
   } ["SCREEN", "LINEAR_LIGHT"].reduce((hexCode, t, n) => Object.assign(hexCode, {
     [t]: n
